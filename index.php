56f9839cf44e4dcc0e641f350438f906ecd90828
9-sho-5 <hosho.kusu@icloud.com>
2022-08-26 21:38:31 +0900

    fix: add comments -> index.php

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 /*
+【 管理画面上で固定ページに適用するテンプレートページ名 】
 Template Name: メインページ
 */
 ?>
+
+<!-- header.phpの読み込み -->
 <? get_header(); ?>
 
 <main>
 
 </main>
 
+<!-- footer.phpの読み込み -->
 <?php get_footer();
