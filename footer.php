9852ad397fed6db819067fc6631605c31999bbbf
9-sho-5 <hosho.kusu@icloud.com>
2022-12-11 14:18:15 +0900

    fix: js dir path in footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
     </footer>
     <?php wp_footer(); ?>
-    <script src="<?php echo get_template_directory_uri() ?>/./assets/javascript/main.js"></script>
+    <script src="<?php echo get_template_directory_uri() ?>/./assets/js/main.js"></script>
 </body>
 
 </html>
