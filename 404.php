01eb1a85b434b246ecd2f6ee6dfaff66d69effac
9-sho-5 <hosho.kusu@icloud.com>
2022-12-22 22:20:00 +0900

    fix: 404.php

--- 404.php
+++ 404.php
@@ -11,7 +11,6 @@ Template Name: 404 Not Found ページ
 <main>
     <h1 style="position: relative; top: 50%; left: 50%; transform: translateX(-50%); display: block; width: 100vw; height: 100vh; text-align: center; line-height: 100vh;">
         <span>404</span>
-        <br>
         <span>Page Not Found</span>
     </h1>
 </main>
