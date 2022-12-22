7f3de8b7f3573c8dc1c413950cd4b2fda55957e6
9-sho-5 <hosho.kusu@icloud.com>
2022-12-23 07:26:36 +0900

    fix: footer toggle

--- footer.php
+++ footer.php
@@ -1,10 +1,115 @@
     <footer>
-
+        <div class="links-wrap">
+            <ul>
+                <li class="anime">
+                    <a href="<?php echo esc_url(home_url('/')); ?>">リンク</a>
+                </li>
+                <li class="anime">
+                    <a href="<?php echo esc_url(home_url('/')); ?>">リンク</a>
+                </li>
+            </ul>
+            <ul>
+                <li class="parent-link anime">
+                    <a>リンク</a>
+                </li>
+                <ul class="sub-link-wrap">
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                </ul>
+            </ul>
+            <ul>
+                <li class="parent-link anime">
+                    <a>リンク</a>
+                </li>
+                <ul class="sub-link-wrap">
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                </ul>
+            </ul>
+            <ul>
+                <li class="parent-link anime">
+                    <a>リンク</a>
+                </li>
+                <ul class="sub-link-wrap">
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                    <li class="sub-link anime">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">サブリンク</a>
+                    </li>
+                </ul>
+            </ul>
+            <ul>
+                <li class="anime">
+                    <a href="<?php echo esc_url(home_url('/')); ?>">リンク</a>
+                </li>
+                <li class="anime">
+                    <a href="<?php echo esc_url(home_url('/')); ?>">リンク</a>
+                </li>
+                <li class="anime">
+                    <a href="<?php echo esc_url(home_url('/')); ?>">リンク</a>
+                </li>
+            </ul>
+            <div class="icons anime">
+                <a href="" class="icon">
+                    <i class="fa-solid fa-envelope"></i>
+                </a>
+                <a href="" class="icon">
+                    <i class="fa-solid fa-location-dot"></i>
+                </a>
+            </div>
+        </div>
+        <p class="sp-cr anime">©COUNT LEAD. ALL RIGHT RESERVED.</p>
     </footer>
     <?php wp_footer(); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="<?php echo get_template_directory_uri() ?>/./assets/js/main.js"></script>
     <script src="<?php echo get_template_directory_uri() ?>/./assets/js/ham_menu.js"></script>
+    <script src="<?php echo get_template_directory_uri() ?>/./assets/js/footer.js"></script>
 </body>
 
 </html>
