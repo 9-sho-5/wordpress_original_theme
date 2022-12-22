8bc9a4d846c358b9c2c6cbc5a0230cfb7fdbf1a0
9-sho-5 <hosho.kusu@icloud.com>
2022-12-23 08:09:00 +0900

    fix: copy right

--- footer.php
+++ footer.php
@@ -118,7 +118,7 @@
                 </a>
             </div>
         </div>
-        <p class="sp-cr anime">©COUNT LEAD. ALL RIGHT RESERVED.</p>
+        <p class="sp-cr anime">©SAMPLE COMPANY. ALL RIGHT RESERVED.</p>
     </footer>
     <?php wp_footer(); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
