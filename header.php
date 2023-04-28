e688b366c42ecd72e4a949129e303d4c003f79b9
9-sho-5 <hosho.kusu@icloud.com>
2023-04-29 05:29:01 +0900

    fix: set page title

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>サイト名</title>
+    <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
